b5435ca30d2b8202d03932a16369a5ef5fe30671
Preranashinde <preranapshinde3009@gmail.com>
2020-03-24 20:46:24 -0300

    Roll a die to get random number between 1 to 6

--- snakeLadderSimulator.sh
+++ snakeLadderSimulator.sh
@@ -5,3 +5,7 @@ position=0
 
 #At start position with single player
 echo "Welcome to Snake And Ladder Game"
+
+#Roll a die to get ranmdom number between 1 to 6
+rollDie=$((RANDOM%6+1))
+echo $rollDie
