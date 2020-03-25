a02620f508e70ebfa6f427b1d10ae232a71a7f51
Preranashinde <preranapshinde3009@gmail.com>
2020-03-25 12:07:18 -0300

    Final game is done

--- snakeLadderSimulator.sh
+++ snakeLadderSimulator.sh
@@ -3,51 +3,70 @@
 #Constants
 POSITION=0
 WINNING_POSITION=100
-#Variables 
-position=0
 noPlay=0
 ladder=1
 snake=2
-echo "Welcome to Snake and Ladder game"
-# Roll a die to get ranmdom number between 1 to 6
+#Variables 
+position=0
+dieCountofPlayer1=0
+dieCountofPlayer2=0
+player1Position=0
+player2Position=0
+echo "Welcome to Snake And Ladder Game"
 #Winning condition
-while(($position!=$WINNING_POSITION))
-do
-	rollDie=$((RANDOM%6+1))
-	echo "Roll die output is:" $rollDie
-	selectOption=$((RANDOM%3))
-	case $selectOption in
+function playSnakeAndLadder()
+{
+	position=$1
+	# Roll a die to get a random number between 1 to 6
+	rollDieOutcome=$((RANDOM%6+1))
+	optionCheck=$((RANDOM%3))
+	#Check options Noplay/Ladder/Snake
+	case $optionCheck in
 		$noPlay)
-			echo "Player current position is:"$position
-			echo "You stay in same position:"$position
 			position=$position
 			;;
 		$ladder)
-			echo "Player current position is :" $position
-			echo "You climb a ladder."
 			# Exact winning condition
-			if(( $((position+rollDie))>100  ))
+			if(( $((position+rollDieOutcome))>100  ))
 			then
 				position=$position
-				echo "Player current position is:"$position
 			else
-				position=$((position+rollDie))
-				echo "New position of player is:"$position
+				position=$((position+rollDieOutcome))
+				position=$position
 			fi
 			;;
 		$snake)
-			echo "You swalloed by snake."
-			echo "Player current position is :" $position
 			# Exact restart condition
-			if(($position<0))
+			if(($position>$rollDieOutcome))
 			then
-				echo "New position of player is:" $POSITION
-				position=$POSITION
-			else
-				position=$((position-rollDie))
-				echo "New position of player is:" $position
+				position=$((position-rollDieOutcome))
 				position=$position
+			else
+				position=$POSITION
 			fi
 			;;
 	esac
+	echo $position
+}
+
+# Play till 100 for both players
+
+while(($positionForPlayer1!=$WINNING_POSITION && $positionForPlayer2!=$WINNING_POSITION))
+do
+	((dieCountofPlayer1++))
+	player1Position="$(playSnakeAndLadder $player1Position)"
+	echo "Player 1 die count $dieCountofPlayer1 : Position $player1Position"
+	((dieCountofPlayer2++))
+	player2Position="$(playSnakeAndLadder $player2Position)"
+	echo "Player 2 die count $dieCountofPlayer1 : Position $player2Position"
 done
+
+# Condition to know which player won
+
+if(($player1Position==$WINNING_POSITION))
+then
+	echo "Congracts!!!!!player 1 won. you rolled the die for $dieCountofPlayer1 times"
+else
+	echo "Congracts!!!!!player 2 won. you rolled the die for $dieCountofPlayer2 times"
+fi
+
